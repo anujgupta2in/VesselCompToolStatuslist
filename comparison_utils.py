13fcc601f1d523c9a832cc80710992c4e1c78d07
anujgupta2in <95157005+anujgupta2in@users.noreply.github.com>
2025-04-04 12:29:37 +0530

    Update comparison_utils.py

--- comparison_utils.py
+++ comparison_utils.py
@@ -405,6 +405,13 @@ def rename_machinery(value):
     r"^Mooring WinchFwd-Port1$": "Mooring Winch F-P1",
     r"^Mooring WinchFwd-Stbd1$": "Mooring Winch F-S1",
 
+
+        # Liferaft FP / FS
+    r"^LiferaftFP$": "Liferaft FP",
+    r"^LiferaftFS$": "Liferaft FS",
+    r"^LiferaftFwd-P$": "Liferaft FP",
+    r"^LiferaftFwdS$": "Liferaft FS",
+
     }
     
     for pattern, replacement in specific_mapping.items():
